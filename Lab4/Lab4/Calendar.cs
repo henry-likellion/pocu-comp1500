1b69bb4c2a89bd02a85611d1feddd84771ba9fce
henry-likellion <33973504+henry-likellion@users.noreply.github.com>
2019-05-28 02:41:04 +1000

    Lab4 first submit

---
+++ Lab4/Lab4/Calendar.cs
@@ -0,0 +1,52 @@
+﻿namespace Lab4
+{
+    public static class Calendar
+    {
+        public static bool IsLeapYear(uint year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            else if (year % 100 == 0)
+            {
+                return false;
+            }
+            else if (year % 4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int GetDaysInMonth(uint year, uint month)
+        {
+            if (year < 10000 && month <= 12)
+            {
+                if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                {
+                    return 31;
+                }
+                else if (month == 4 || month == 6 || month == 9 || month == 11)
+                {
+                    return 30;
+                }
+                else if (month == 2)
+                {
+                    if (Calendar.IsLeapYear(year))
+                    {
+                        return 29;
+                    }
+                    else
+                    {
+                        return 28;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
