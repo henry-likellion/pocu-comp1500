ebdbcc2525fb544b7c0df3bebd38cd773b9a514d
henry-likellion <33973504+henry-likellion@users.noreply.github.com>
2019-07-05 00:39:12 +1000

    Lab7 first attempt

---
+++ Lab7/Lab7/Lab7.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Lab7
+{
+    public static class Lab7
+    {
+        public static bool PlayGame(uint[] array)
+        {
+            int lengthArray = array.Length;
+            bool[] hasVisited = new bool[lengthArray];
+
+            for (int i = 0; i < lengthArray; ++i)
+            {
+                hasVisited[i] = false;
+            }
+
+            if (lengthArray < 2)
+            {
+                return false;
+            }
+
+            if (array[0] > array.Length - 1)
+            {
+                return false;
+            }
+
+            return Move(array, hasVisited, array[0]);
+        }
+
+        public static bool Move(uint[] array, bool[] hasVisited, uint currentPosition)
+        {
+            if (array[currentPosition] == 0)
+            {
+                return true;
+            }
+
+            if (hasVisited[currentPosition])
+            {
+                return false;
+            }
+            else
+            {
+                hasVisited[currentPosition] = true;
+            }
+
+
+            uint newPositionRight = currentPosition + array[currentPosition];
+
+            if (newPositionRight <= array.Length - 1)
+            {
+                if (Move(array, hasVisited, newPositionRight))
+                {
+                    return true;
+                }
+            }
+            
+            if (currentPosition > array[currentPosition])
+            {
+                uint newPositionLeft = currentPosition - array[currentPosition];
+                if (Move(array, hasVisited, newPositionLeft))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
