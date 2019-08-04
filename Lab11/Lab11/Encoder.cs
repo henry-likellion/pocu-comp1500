3963941a9cb6d2e84f7a3581a29a4b14a4fb2b54
henry-likellion <33973504+henry-likellion@users.noreply.github.com>
2019-08-05 03:08:17 +1000

    Lab 11 third attempt leaveOpen

--- Lab11/Lab11/Encoder.cs
+++ Lab11/Lab11/Encoder.cs
@@ -13,7 +13,7 @@ namespace Lab11
                 return false;
             }
 
-            var writer = new BinaryWriter(output);
+            var writer = new BinaryWriter(output, System.Text.Encoding.ASCII, false);
             
             int asciiNumber = input.ReadByte();
             byte counter = 1;
@@ -43,7 +43,6 @@ namespace Lab11
                     counter = 1;
                 }
             }
-            writer.Dispose();     
 
             return true;
         }
@@ -55,8 +54,8 @@ namespace Lab11
                 return false;
             }
 
-            var writer = new StreamWriter(output);
-            
+            var writer = new StreamWriter(output, System.Text.Encoding.ASCII, 512 ,false);
+
             int counter = input.ReadByte();
 
             for (int i = 1; i < input.Length; ++i)
@@ -79,9 +78,7 @@ namespace Lab11
 
             input.Seek(0, SeekOrigin.Begin);
             output.Seek(0, SeekOrigin.Begin);
-
-            writer.Dispose();
-
+            
             return true;
         }
     }
