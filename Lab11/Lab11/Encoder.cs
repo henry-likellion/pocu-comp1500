90acd6b0fba8430b155e29d6d918e8839b0cf4fd
henry-likellion <33973504+henry-likellion@users.noreply.github.com>
2019-08-05 04:42:05 +1000

    fix error - seek issue

--- Lab11/Lab11/Encoder.cs
+++ Lab11/Lab11/Encoder.cs
@@ -16,7 +16,7 @@ namespace Lab11
             var writer = new BinaryWriter(output, System.Text.Encoding.ASCII, false);
             
             int asciiNumber = input.ReadByte();
-            byte counter = 1;
+            int counter = 1;
 
             for (int i = 1; i <= input.Length; ++i)
             {
@@ -24,19 +24,26 @@ namespace Lab11
 
                 if (i == input.Length)
                 {
-                    writer.Write(counter);
+                    writer.Write((byte)counter);
                     writer.Write((byte)asciiNumber);
-                    output.Seek(0, SeekOrigin.Begin);
-                    input.Seek(0, SeekOrigin.Begin);
                 }
 
                 if (asciiNumber == value)
                 {
                     ++counter;
+                    if (counter > 255)
+                    {
+                        writer.Write((byte)255);
+                        writer.Write((byte)asciiNumber);
+                        counter = 1;
+                    }
                 }
                 else
                 {
-                    writer.Write(counter);
+                    Console.WriteLine(counter);
+                    Console.WriteLine(asciiNumber);
+                    
+                    writer.Write((byte)counter);
                     writer.Write((byte)asciiNumber);
 
                     asciiNumber = value;
@@ -45,6 +52,9 @@ namespace Lab11
             }
 
             writer.Flush();
+
+            input.Seek(0, SeekOrigin.Begin);
+            output.Seek(0, SeekOrigin.Begin);
 
             return true;
         }
@@ -56,7 +66,7 @@ namespace Lab11
                 return false;
             }
 
-            var writer = new StreamWriter(output, System.Text.Encoding.ASCII, 512 ,false);
+            var writer = new StreamWriter(output, System.Text.Encoding.ASCII, 512, false);
 
             int counter = input.ReadByte();
 
