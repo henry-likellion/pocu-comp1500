5f3a1e81651e568936b370f3a3b1c0b6be7b298c
henry-likellion <33973504+henry-likellion@users.noreply.github.com>
2019-08-05 03:26:47 +1000

    Lab 13 4th attempt add flush

--- Lab11/Lab11/Encoder.cs
+++ Lab11/Lab11/Encoder.cs
@@ -44,6 +44,8 @@ namespace Lab11
                 }
             }
 
+            writer.Flush();
+
             return true;
         }
 
@@ -54,7 +56,7 @@ namespace Lab11
                 return false;
             }
 
-            var writer = new StreamWriter(output, System.Text.Encoding.ASCII, 512 ,false);
+            var writer = new StreamWriter(output);
 
             int counter = input.ReadByte();
 
@@ -75,6 +77,8 @@ namespace Lab11
                     }
                 }
             }
+
+            writer.Flush();
 
             input.Seek(0, SeekOrigin.Begin);
             output.Seek(0, SeekOrigin.Begin);
