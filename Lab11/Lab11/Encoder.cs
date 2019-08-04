426658ad556eca0cd82cd3805aeca9f9a1cd724d
henry-likellion <33973504+henry-likellion@users.noreply.github.com>
2019-08-05 01:32:40 +1000

    Lab 11 first attempt

---
+++ Lab11/Lab11/Encoder.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System;
+
+
+namespace Lab11
+{
+    public static class Encoder
+    {
+        public static bool TryEncode(Stream input, Stream output)
+        {
+            if (input.Length == 0)
+            {
+                return false;
+            }
+
+            int asciiNumber = input.ReadByte();
+            byte counter = 1;
+
+            using (var writer = new BinaryWriter(output))
+            {
+                for (int i = 1; i <= input.Length; ++i)
+                {
+                    int value = input.ReadByte();
+
+                    if (i == input.Length)
+                    {
+
+                        writer.Write(counter);
+                        writer.Write((byte)asciiNumber);
+                    }
+
+                    if (asciiNumber == value)
+                    {
+                        ++counter;
+                    }
+                    else
+                    {
+                        writer.Write(counter);
+                        writer.Write((byte)asciiNumber);
+
+                        asciiNumber = value;
+                        counter = 1;
+                    }
+                }
+            }
+            
+            return true;
+        }
+
+        public static bool TryDecode(Stream input, Stream output)
+        {
+            if (input.Length == 0)
+            {
+                return false;
+            }
+
+            using (var writer = new StreamWriter(output))
+            {
+                int counter = input.ReadByte();
+
+                for (int i = 1; i < input.Length; ++i)
+                {
+                    if (i % 2 == 0)
+                    {
+                        counter = input.ReadByte();
+                    }
+                    else
+                    {
+                        int characterInt = input.ReadByte();
+                        char character = (char)characterInt;
+
+                        for (int j = 0; j < counter; ++j)
+                        {
+                            writer.Write(character);
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
