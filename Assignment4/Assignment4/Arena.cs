ea920aa7cf3613b8ca5895134c077755a4ad517f
henry-likellion <33973504+henry-likellion@users.noreply.github.com>
2019-08-11 21:42:19 +1000

    Ass 4 first attempt

---
+++ Assignment4/Assignment4/Arena.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Assignment4
+{
+    public class Arena
+    {
+        public string ArenaName { get;  }
+        public uint Capacity { get; }
+        public uint Turns { get; private set; }
+        public uint MonsterCount { get; private set; }
+
+        public List<Monster> Monsters { get; private set; }
+
+        public Arena(string arenaName, uint capacity)
+        {
+            ArenaName = arenaName;
+            Capacity = capacity;
+            MonsterCount = 0;
+            Turns = 0;
+            Monsters = new List<Monster>();
+        }
+
+        public void LoadMonsters(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return;
+            }
+
+            using(var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    if (Capacity == MonsterCount)
+                    {
+                        break;
+                    };
+
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    string name = values[0];
+                    EElementType elementType;
+
+                    if (values[1] == "Earth")
+                    {
+                        elementType = EElementType.Earth;
+                    }
+                    else if (values[1] == "Water")
+                    {
+                        elementType = EElementType.Water;
+                    }
+                    else if (values[1] == "Wind")
+                    {
+                        elementType = EElementType.Wind;
+                    }
+                    else
+                    {
+                        elementType = EElementType.Fire;
+                    }
+
+                    int health = int.Parse(values[2]);
+                    int attack = int.Parse(values[3]);
+                    int defense = int.Parse(values[4]);
+
+                    Monster newMonster = new Monster(name, elementType, health, attack, defense);
+
+                    Monsters.Add(newMonster);
+                    ++MonsterCount;
+                }
+            }
+        }
+
+        public void GoToNextTurn()
+        {
+            int countMonsters = Monsters.Count;
+            List<int> indexesOut = new List<int>(countMonsters);
+
+            if (countMonsters <= 1)
+            {
+                return;
+            }
+
+            for (int i = 0; i < countMonsters; ++i)
+            {
+                Monster monster = Monsters[i];
+
+                if (monster.Health > 0)
+                {
+                    int index;
+                    if (i == countMonsters - 1)
+                    {
+                        index = 0;
+                    }
+                    else
+                    {
+                        index = i + 1;
+                    }
+
+                    Monster nextMonster = Monsters[index];
+
+                    monster.Attack(nextMonster);
+
+                    if (nextMonster.Health == 0)
+                    {
+                        indexesOut.Add(index);
+                    }
+                }
+            }
+
+            foreach(int index in indexesOut)
+            {                
+                Monsters.RemoveAt(index);
+                --MonsterCount;
+            }
+            ++Turns;
+            Console.WriteLine(Turns);
+        }
+
+        public Monster GetHealthiest()
+        {
+            if (MonsterCount == 0)
+            {
+                return null;
+            }
+            int indexHealthiest = 0;
+
+            for(int i = 1; i < Monsters.Count; ++i)
+            {
+                if (Monsters[indexHealthiest].Health < Monsters[i].Health)
+                {
+                    indexHealthiest = i;
+                }
+            }
+            return Monsters[indexHealthiest];
+        }
+    }
+}
---
+++ Assignment4/Assignment4/Monster.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment4
+{
+    public class Monster
+    {
+        public string Name { get; }
+        public EElementType ElementType { get; }
+        public int Health { get; private set; }
+        public int AttackStat { get; }
+        public int DefenseStat { get; }
+        public Monster(string name, EElementType elementType, int health, int attack, int defense)
+        {
+            Name = name;
+            ElementType = elementType;
+            Health = health;
+            AttackStat = attack;
+            DefenseStat = defense;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            if (Health < amount)
+            {
+                Health = 0;
+            }
+            else
+            {
+                Health -= amount;
+            }
+        }
+
+        public void Attack(Monster otherMonster)
+        {   
+            int damage = AttackStat - otherMonster.DefenseStat;
+
+            switch (ElementType)
+            {
+                case EElementType.Fire:
+                    switch (otherMonster.ElementType)
+                    {
+                        case (EElementType.Wind):
+                            damage = (int)(damage * 1.5);
+                            break;
+                        case (EElementType.Water):
+                            damage = (int)(damage * 0.5);
+                            break;
+                        case (EElementType.Earth):
+                            damage = (int)(damage * 0.5);
+                            break;
+                    }
+                    break;
+                case EElementType.Wind:
+                    switch (otherMonster.ElementType)
+                    {
+                        case (EElementType.Earth):
+                            damage = (int)(damage * 1.5);
+                            break;
+                        case (EElementType.Water):
+                            damage = (int)(damage * 1.5);
+                            break;
+                        case (EElementType.Fire):
+                            damage = (int)(damage * 0.5);
+                            break;
+                    }
+                    break;
+                case EElementType.Water:
+                    switch (otherMonster.ElementType)
+                    {
+                        case (EElementType.Fire):
+                            damage = (int)(damage * 1.5);
+                            break;
+                        case (EElementType.Wind):
+                            damage = (int)(damage * 0.5);
+                            break;
+                    }
+                    break;
+                case EElementType.Earth:
+                    switch (otherMonster.ElementType)
+                    {
+                        case (EElementType.Fire):
+                            damage = (int)(damage * 1.5);
+                            break;
+                        case (EElementType.Wind):
+                            damage = (int)(damage * 0.5);
+                            break;
+                    }
+                    break;
+            }
+
+            if (damage < 1)
+            {
+                damage = 1;
+            }
+
+            otherMonster.TakeDamage(damage);
+        }
+    }
+}
